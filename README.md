e6cbb09a23b856f07cdc083b589c727157dba6e0
David Ackerman <145808634+dm-ackerman@users.noreply.github.com>
2024-01-25 22:58:14 +0000

    initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+This is knight-archers-zombies from pettingzoo, just with a different name
+
+There is nothing interesting here
+
---
+++ knights_archers_decepticons_v0.py
@@ -0,0 +1,8 @@
+from knights_archers_decepticons.knights_archers_decepticons import (
+    ManualPolicy,
+    env,
+    parallel_env,
+    raw_env,
+)
+
+__all__ = ["ManualPolicy", "env", "parallel_env", "raw_env"]
